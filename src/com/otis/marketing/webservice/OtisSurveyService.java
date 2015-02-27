cec6c2fbf23553bfef5536914b5672dd8c9055e1
bradencao <bradencao@hotmail.com>
2015-02-27 11:47:59 +0800

    Added web service interface for survey

---
+++ src/com/otis/marketing/webservice/OtisSurveyService.java
@@ -0,0 +1,22 @@
+package com.otis.marketing.webservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/otisService")
+public interface OtisSurveyService {
+
+	@GET
+	@Path("/getSurvey")
+	@Produces( {MediaType.APPLICATION_JSON} )
+	public String getSurveyById(@QueryParam("surveyId") String surveyId);
+	
+	@POST
+	@Path("/")
+	@Produces( {MediaType.APPLICATION_JSON} )
+	public String submitReply(@QueryParam("content") String content);
+}
