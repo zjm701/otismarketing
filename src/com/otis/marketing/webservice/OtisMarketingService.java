9cc59ba8d314554a00900cc584aa97fa9cffaa05
bradencao <bradencao@hotmail.com>
2015-03-04 22:57:44 +0800

    Optimize web service - Merged the codes

--- src/com/otis/marketing/webservice/OtisMarketingService.java
+++ src/com/otis/marketing/webservice/OtisMarketingService.java
@@ -1,8 +1,10 @@
 package com.otis.marketing.webservice;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
@@ -18,4 +20,14 @@ public interface OtisMarketingService {
 	@Path(value = "/getNewsDetails")
 	@Produces({ MediaType.APPLICATION_JSON })
 	public String getNewsDetails(@QueryParam("newsId") String newsId);
+	
+	@GET
+	@Path(value = "/getSurvey")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String getSurveyById(@QueryParam("surveyId") String surveyId);
+	
+	@POST
+	@Path(value = "/submitReply")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public String submitReply(@QueryParam("content") String content);
 }
--- src/com/otis/marketing/webservice/OtisMarketingServiceImpl.java
+++ src/com/otis/marketing/webservice/OtisMarketingServiceImpl.java
@@ -1,16 +1,30 @@
 package com.otis.marketing.webservice;
 
+import java.lang.reflect.Type;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.otis.marketing.entity.News;
+import com.otis.marketing.entity.Survey;
 import com.otis.marketing.service.NewsService;
+import com.otis.marketing.service.SurveyService;
 import com.otis.marketing.utils.Utils;
 
 public class OtisMarketingServiceImpl implements OtisMarketingService {
 
 	@Autowired
 	private NewsService newsService;
+	
+	@Autowired
+	private SurveyService surveyService;
 	
 	@Override
 	public String sayHello(String name) {
@@ -31,5 +45,38 @@ public class OtisMarketingServiceImpl implements OtisMarketingService {
 		json.addProperty("createTime", Utils.formateDateTime(news.getCreateTime()));
 		
 		return json.toString();
+	}
+	
+	@Override
+	public String getSurveyById(String surveyId) {
+		Survey survey = surveyService.getById(Integer.parseInt(surveyId));
+		GsonBuilder builder = new GsonBuilder();
+		builder.excludeFieldsWithoutExposeAnnotation();
+		builder.serializeNulls();
+		builder.registerTypeAdapter(Date.class, new JsonSerializer<Date>(){
+			public JsonElement serialize(Date date, Type type,  
+	                JsonSerializationContext arg2) {  
+	                return new JsonPrimitive(Utils.formateDate(date));
+	        }
+		});
+		Gson gson = builder.create();
+		return gson.toJson(survey);
+	}
+	
+	@Override
+	public String submitReply(String content) {
+		try{
+		}catch(Exception e){
+			return getFailedJsonResult();
+		}
+		return getSuccessfulJsonResult();
+	}
+	
+	private String getSuccessfulJsonResult() {
+		return "{\"result\":\"successful\"}";
+	}
+	
+	private String getFailedJsonResult() {
+		return "{\"result\":\"failed\"}";
 	}
 }
